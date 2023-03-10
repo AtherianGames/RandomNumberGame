98542d9e47cad08e9accdbe26ee36d72bcc8bcb8
ejross42 <ejross42@tntech.edu>
2023-03-09 22:10:11 -0600

    Clean Up and Menu Class Interface

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 import random
 import array as arr
+from collections import defaultdict
 import math
+
+class MenuInterface(object):
+    def runUserSelectedOption(self, user_option):
+        raise NotImplementedError()
+    def displayMenu(self): #Virtual function - required to be overwritten by derived class
+        raise NotImplementedError()
+    def getUserOption(self):
+        return int(input("Which option would you like to select? "))
+
+    def runMenu(self):
+        self.displayMenu()
+        self.runUserSelectedOption(self.getUserOption())
+
+    '''@staticmethod
+    def runMenu(menu_interface):
+        menu_interface.displayMenu()'''
 
 def get_random_num(b):
     return random.randint(0,b)
+class MainMenu(MenuInterface):
+    def runUserSelectedOption(self, user_option):
+        {
+            1: random_number_game,
+            2: display_alg_menu,
+            3: exit
+        }[user_option]()
+    def displayMenu(self):
+        print("******** Random Number Game ********")
+        print("Would you like to play the game or see a computer algorithm?")
+        print("1. Play a Game.")
+        print("2. See a Computer Algorithm.")
+        print("3. Exit")
+
+def display_alg_menu():
+    print("What kind of computer algorithm would you like to see?")
+    print("1. Random Placement")
+    print("2. Percentile Exclusive Placement")
+    print("3. Percentile & Bell Curve Distribution Placement")
+    ch2 = int(input("Which option would you like to select? "))
+    return ch2
 
 def set_bounds():
     arrsize = 0
@@ -17,12 +55,21 @@ def set_bounds():
     return arrsize, bound2
 
 def print_set_nums(set_nums):
-    for x in set_nums:
-        if x == -1:
-            print("___", end=" ")
-        else:
-            print(x, end=" ")
-    print("")
+
+    #mydict = defaultdict(lambda: "___")
+
+    #print(' '.join([mydict[x] for x in range(len(set_nums))]))
+
+    print(' '.join(["___" if x == -1 else str(x) for x in set_nums]))
+
+'''
+    # for x in set_nums:
+    #     if x == -1:
+    #         print("___", end=" ")
+    #     else:
+    #         print(x, end=" ")
+    # print("")
+'''
 
 def random_number_game():
     bounds = set_bounds()
@@ -44,13 +91,16 @@ def random_number_game():
                 pass
             else:
                 if x < lastx:
+                    print_set_nums(set_nums)
                     print("You have lost the game!")
                     return None
                 else:
                     lastx = x
         roundcount += 1
+    print_set_nums(set_nums)
+    print("You won the game.")
 
-'''def optimal_percentile_game():
+def optimal_percentile_game():
     bounds = set_bounds()
     arrsize = bounds[0]
     bound2 = bounds[1]
@@ -87,7 +137,7 @@ def random_number_game():
                     lastx = x
         roundcount += 1
     print_set_nums(set_nums)
-    print("You won the game.")'''
+    print("You won the game.")
 
 def random_number_alg():
     bounds = set_bounds()
@@ -119,31 +169,32 @@ def random_number_alg():
                     lastx = x
         roundcount += 1
 
-while 1:
-    print("******** Random Number Game ********")
-    print("Would you like to play the game or see a computer algorithm?")
-    print("1. Play a Game.")
-    print("2. See a Computer Algorithm.")
-    print("3. Exit")
-    ch = int(input("Which option would you like to select? "))
-    if (ch == 1):
-        random_number_game()
-    elif (ch == 2):
-        print("What kind of computer algorithm would you like to see?")
-        print("1. Random Placement")
-        print("2. Percentile Exclusive Placement")
-        print("3. Percentile & Bell Curve Distribution Placement")
-        ch2 = int(input("Which option would you like to select? "))
-        if ch2 == 1:
-            random_number_alg()
-        elif ch2 == 2:
-            #optimal_percentile_game()
-            print("Not Implemented Yet")
-        elif ch2 == 3:
-            print("Not Implemented Yet")
+def main():
+
+    #holds values indefinitely
+    main_menu = MainMenu()
+    while 1:
+        main_menu.runMenu()
+        #MainMenu.runMenu(main_menu)
+        '''ch = display_main_menu()
+        if (ch == 1):
+        # rename - Launching game instance
+            random_number_game()
+        # launch algorithm game
+        elif (ch == 2):
+            ch2 = display_alg_menu()
+            if ch2 == 1:
+                random_number_alg()
+            elif ch2 == 2:
+                optimal_percentile_game()
+            elif ch2 == 3:
+                print("Not Implemented Yet")
+            else:
+                print("Invalid Selection.")
+        elif (ch == 3):
+            exit()
         else:
-            print("Invalid Selection.")
-    elif (ch == 3):
-        exit()
-    else:
-        print("Invalid Selection.")
+            print("Invalid Selection.")'''
+
+if __name__ == "__main__":
+    main()
